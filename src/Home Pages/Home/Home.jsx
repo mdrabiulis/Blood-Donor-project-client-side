0352c55c649323443fd0c47deea5a0232968b6ff
mdrabiulis <mdrabiulis155@gmail.com>
2024-04-02 21:33:29 +0600

    Banner desing

--- src/Home Pages/Home/Home.jsx
+++ src/Home Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             Home
         </div>
     );
