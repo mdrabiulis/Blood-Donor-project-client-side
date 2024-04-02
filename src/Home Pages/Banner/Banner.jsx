fa962f75ac43e7a438ebefc39b3b1a2d0c81df75
mdrabiulis <mdrabiulis155@gmail.com>
2024-04-02 23:53:15 +0600

    Banner desing swiper

--- src/Home Pages/Banner/Banner.jsx
+++ src/Home Pages/Banner/Banner.jsx
@@ -1,45 +1,46 @@
-
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-import img from "../../assets/Banner.jpg"
-import img1 from "../../assets/Banner1.jpg"
-import img2 from "../../assets/Banner2.jpg"
+import img from "../../assets/Banner.jpg";
+import img1 from "../../assets/Banner1.jpg";
+import img2 from "../../assets/Banner2.jpg";
 const Banner = () => {
-    return (
-        <>
-        <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper"
-        >
-          <SwiperSlide><img  src={img} alt="" /></SwiperSlide>
-          <SwiperSlide><img src={img1} alt="" /></SwiperSlide>
-          <SwiperSlide><img src={img2} alt="" /></SwiperSlide>
-
-
-        </Swiper>
-      </>
-    );
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img className="w-[1600px] h-[900px]" src={img} alt="" />
+        </SwiperSlide>
+        {/* <SwiperSlide>
+          <img className="w-[850px] " src={img1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-[850px] " src={img2} alt="" />
+        </SwiperSlide> */}
+      </Swiper>
+    </>
+  );
 };
 
 export default Banner;
